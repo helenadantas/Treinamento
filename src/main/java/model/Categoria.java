094b8e1d048d46dfc03a55bd32cf408c11c90b43
Helena Dantas <helena.dantas@ccc.ufcg.edu.br>
2019-11-26 19:55:45 -0300

    Treinamento

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -1,8 +1,16 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
 public class Categoria {
+	@Id
 	private Long id;
 	private String name;
+	
 	public Long getId() {
 		return id;
 	}
